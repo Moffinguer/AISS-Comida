c59f502ea9902557cfd0e6af3c03b45bd1a62f96
Moffinguer <fermago11@gmail.com>
2022-05-01 10:27:10 +0200

    Nuevo objeto ingrediente, en vez de mapa

---
+++ src/main/java/aiss/model/Ingrediente.java
@@ -0,0 +1,24 @@
+package aiss.model;
+
+public class Ingrediente {
+	private Alimento alimento;
+	private Double cantidad;
+	public Ingrediente(Alimento alimento, Double cantidad) {
+		super();
+		this.alimento = alimento;
+		this.cantidad = cantidad;
+	}
+	public Alimento getAlimento() {
+		return alimento;
+	}
+	public void setAlimento(Alimento alimento) {
+		this.alimento = alimento;
+	}
+	public Double getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Double cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+}
