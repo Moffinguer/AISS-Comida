209cfa14f088901548f517eb0cb7b13c0db9c50a
Moffinguer <fermago11@gmail.com>
2022-05-22 09:44:29 +0200

    Test login y add tokens

--- src/test/java/aiss/model/resources/TestRecetaExterna.java
+++ src/test/java/aiss/model/resources/TestRecetaExterna.java
@@ -7,16 +7,35 @@ import static org.junit.Assert.assertTrue;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import aiss.api.resources.PerfilResource;
 import aiss.api.resources.RecetaResource;
+import aiss.model.PerfilExterno;
 import aiss.model.RecetaExterna;
 
 public class TestRecetaExterna {
 	
-	public RecetaExterna recetaAux= new RecetaExterna();
-	public RecetaExterna recetaPost= new RecetaExterna();
+	public RecetaExterna recetaAux= new RecetaExterna(),
+		                 recetaPost= new RecetaExterna();
 	static RecetaResource resourceReceta=new RecetaResource();
+	static PerfilResource resourcePerfil = new PerfilResource();
+	public static String API_KEY;
+	@BeforeClass
+	public static void getKey() {
+		String messageKey = resourcePerfil.addProfile(new PerfilExterno(
+				"AissProyect",
+				"618587795",
+				"aissProyect@us.es")
+				);
+		String api_key = resourcePerfil.getKey();
+		assertNotNull("No hemos creado una clave, mal formato a la hora de hacer la petición", messageKey);
+		assertNotNull("No se ha podido recuperar la clave para operar con nuestras recetas", api_key);
+		System.out.println(api_key);
+		API_KEY = api_key;
+	}
 	
 	@Test
 	public void testGetAll() {
@@ -30,30 +49,30 @@ public class TestRecetaExterna {
 		}
 	}
 	
-	@Test
-	public void testAddPlaylist() {
-			recetaAux.setId("r3");
-			recetaAux.setCalorias("203");
-			recetaAux.setDescripcion("pizza con tomate");
-			recetaAux.setCategoria("italiana");
-			recetaAux.setPersonas("3");
-			recetaAux.setPrecio("2");
-			recetaAux.setTiempo("90");
-			recetaAux.setNombre("pizza");
-			
-			recetaPost = resourceReceta.addReceta(recetaAux);
-			
-			assertNotNull("Error al sumar la receta", recetaPost);
-			assertEquals("Error en el id de la receta", "r3", recetaPost.getId());
-			assertEquals("Error en el nombre de la receta", "pizza", recetaPost.getNombre());
-			assertEquals("Error en las calorías de la receta", "203", recetaPost.getCalorias());
-			assertEquals("Error en la descripcion de la receta", "pizza con tomate", recetaPost.getDescripcion());
-			assertEquals("Error en la categoría de la receta", "italiana", recetaPost.getCategoria());
-			assertEquals("Error en las personas de la receta", "3", recetaPost.getPersonas());
-			assertEquals("Error en el precio de la receta", "2", recetaPost.getPrecio());
-			assertEquals("Error en el tiempo de la receta", "90", recetaPost.getTiempo());
-			
-		}
+//	@Test
+//	public void testAddPlaylist() {
+//			recetaAux.setId("r3");
+//			recetaAux.setCalorias("203");
+//			recetaAux.setDescripcion("pizza con tomate");
+//			recetaAux.setCategoria("italiana");
+//			recetaAux.setPersonas("3");
+//			recetaAux.setPrecio("2");
+//			recetaAux.setTiempo("90");
+//			recetaAux.setNombre("pizza");
+//			
+//			recetaPost = resourceReceta.addReceta(recetaAux, API_KEY);
+//			
+//			assertNotNull("Error al sumar la receta", recetaPost);
+//			assertEquals("Error en el id de la receta", "r3", recetaPost.getId());
+//			assertEquals("Error en el nombre de la receta", "pizza", recetaPost.getNombre());
+//			assertEquals("Error en las calorías de la receta", "203", recetaPost.getCalorias());
+//			assertEquals("Error en la descripcion de la receta", "pizza con tomate", recetaPost.getDescripcion());
+//			assertEquals("Error en la categoría de la receta", "italiana", recetaPost.getCategoria());
+//			assertEquals("Error en las personas de la receta", "3", recetaPost.getPersonas());
+//			assertEquals("Error en el precio de la receta", "2", recetaPost.getPrecio());
+//			assertEquals("Error en el tiempo de la receta", "90", recetaPost.getTiempo());
+//			
+//		}
 	
 	@Test
 	public void testUpdateReceta() {
@@ -66,7 +85,7 @@ public class TestRecetaExterna {
 		receta.setCategoria("nice food");
 		receta.setDescripcion("this is a new description for the recipe");
 		
-		boolean success = RecetaResource.updateReceta(receta);
+		boolean success = RecetaResource.updateReceta(receta, API_KEY);
 		assertTrue("Error when updating the recipe", success);
 		
 		RecetaExterna updatedReceta = null;
